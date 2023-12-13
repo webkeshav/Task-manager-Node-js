b617bded30325818b21314d4feb307d3346a1eae
Keshav <keshavtomar999@gmail.com>
2023-12-13 15:42:37 +0530

    set up get routes

--- starter/app.js
+++ starter/app.js
@@ -1,5 +1,16 @@
 const express = require('express')
 const app = express()
+const tasks = require('./routes/tasks')
+
+
+// middleware
+app.use(express.json())   // if we don't use this them we dont have data in req.body
+
+app.get('/hello',(rq,res)=>{
+    res.send("Task Manager app")
+})
+
+app.use('/api/v1/tasks',tasks)
 
 const port = 3000
 
