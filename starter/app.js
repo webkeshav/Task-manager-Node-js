dde493fe44d43b61607c9ddfbf6f4af807d0acb1
Keshav <keshavtomar999@gmail.com>
2023-12-15 23:17:43 +0530

    try catch wrapper

--- starter/app.js
+++ starter/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
+const notFound = require('./middleware/not-found')
 require('dotenv').config()
 
 
@@ -11,10 +12,12 @@ app.use(express.static("./public"))
 app.use(express.json())   // if we don't use this them we dont have data in req.body
 
 
-
+// routes
 app.use('/api/v1/tasks',tasks)
+app.use(notFound)
 
 const port = 3000
+
 
 const start = async()=>{
     try{await connectDB(process.env.MONGO_URI)
--- starter/controllers/tasks.js
+++ starter/controllers/tasks.js
@@ -1,26 +1,24 @@
 const Task = require('../models/Tasks')
+const asyncWrapper = require('../middleware/async')
 
-const getAllTasks = async(req,res)=>{
-    try {
-        const tasks = await Task.find({})
-        res.status(200).json({tasks})
-    } catch (error) {
-        res.status(500).json({msg:error})
+const getAllTasks = asyncWrapper( async(req,res)=>{
+    
+            const tasks = await Task.find({})
+            res.status(200).json({tasks})
+    
     }
-}
+)
 
-const createTask = async (req,res)=>{
-    try {
+const createTask = asyncWrapper(async (req,res)=>{
+  
         const task = await Task.create(req.body)
         res.status(201).json({task})
         
-    } catch (error) {
-        res.status(500).json({msg:error})
-    }
-}
+   
+})
 
-const getTask = async(req,res)=>{
-    try {
+const getTask = asyncWrapper(async(req,res)=>{
+   
         const {id:taskID} = req.params
         const task = await Task.findOne({_id: taskID})
         if(!task){
@@ -28,30 +26,25 @@ const getTask = async(req,res)=>{
         }
         res.status(200).json({task})
         
-    } catch (error) {
-        res.status(500).json({msg:error})
-    }
    
-}
+})
 
 
-const deleteTask = async(req,res)=>{
-    try {
+const deleteTask = asyncWrapper(async(req,res)=>{
+    
         const {id:taskID} = req.params
         const task = await Task.findOneAndDelete({_id: taskID})
         if(!task){
             return res.status(404).json({msg:`No task with id:${taskID}`})
         }
         res.status(200).json({task})
-    } catch (error) {
-        res.status(500).json({msg:error})
-    }
+   
     
-}
+})
 
 
-const updateTask = async(req,res)=>{
-    try {
+const updateTask = asyncWrapper(async(req,res)=>{
+  
         const {id:taskID} = req.params
         const task = await Task.findOneAndUpdate({_id:taskID},req.body,{
             new:true,
@@ -61,11 +54,9 @@ const updateTask = async(req,res)=>{
             return res.status(404).json({msg:`No task with id:${taskID}`})
         }
         res.status(200).json({task})
-    } catch (error) {
-        res.status(500).json({msg:error})
-    }
+ 
     
-}
+})
 
 module.exports ={
     getAllTasks,
